9f78db5bc2809a5be2f6c439744133dcd0b75c34
Juan Gaybre <55895001+Gaybre@users.noreply.github.com>
2021-03-20 23:34:14 -0600

    Loader (#6)
    * Se agregó el favicon
    
    * Se agrego el componente Loader y se insertó en Home

---
+++ src/components/Loader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className="center">
+      <div class="loader">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default Loader
